5542c2f15cf31814209670417ae39a5287eca606
davidmarshall15 <davidmarshall15@gmail.com>
2016-05-19 19:11:53 +0100

    Demon Class Debuffs
    Added Demon Class to Ignore Debuffs Array.

--- HealBot/HealBot.lua
+++ HealBot/HealBot.lua
@@ -77,6 +77,8 @@ local HealBot_Ignore_Class_Debuffs = {
 						  [HEALBOT_DEBUFF_UNSTABLE_AFFLICTION] = true,},
 	
 	["MONK"] = { [HEALBOT_DEBUFF_UNSTABLE_AFFLICTION] = true,},
+    
+    ["DEMO"] = { [HEALBOT_DEBUFF_UNSTABLE_AFFLICTION] = true,},
 };
 
 local HealBot_Ignore_Movement_Debuffs = {
